cacc5692e0309e791e65b7520e3428c12902c9d6
NathanOllichon <ollichon.nathan@gmail.com>
2023-04-14 19:23:37 +0200

    add models for nation and participation

---
+++ src/app/core/models/nation.model.ts
@@ -0,0 +1,9 @@
+export class Nation {
+    country!: string;
+    id!: number;        //TODO search if number accept decimal, it's the best we can used ?
+    participations!: []
+  }
+
+  // "id": 3,
+  // "country": "United States",
+  // "participations": [
---
+++ src/app/core/models/participation.model.ts
@@ -0,0 +1,13 @@
+export class Participation {
+    id!: number;
+    year!: number;
+    city!: string;
+    medalsCount!: number;
+    athleteCount!: number;
+  }
+
+  // "id": 3,
+  // "year": 2020,
+  // "city": "Tokyo",
+  // "medalsCount": 113,
+  // "athleteCount": 626
