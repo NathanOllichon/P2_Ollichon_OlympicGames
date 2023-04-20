24e4d2f7d948c808a2fb7e1d03920c97b7a40f2b
NathanOllichon <ollichon.nathan@gmail.com>
2023-04-20 19:39:06 +0200

    Add fakes datas for charts

---
+++ src/app/pages/home/total-medals-chart/fakes-datas/FakeNationDatas.ts
@@ -0,0 +1,20 @@
+export var multi = [
+    {
+      name: 'Germany',
+      series: [
+        {
+          name: '1990',
+          value: 64,
+        },
+        {
+          name: '2010',
+          value: 40,
+        },
+        {
+          name: '2011',
+          value: 47,
+        },
+      ],
+    },
+  ];
+  
---
+++ src/app/pages/home/total-medals-chart/fakes-datas/FakeTotalDatas.ts
@@ -0,0 +1,40 @@
+export var single = [
+  {
+    name: 'United States',
+    value: 50000,
+    extra: {
+      code: 'us',
+    },
+  },
+  {
+    name: 'France',
+    value: 36745,
+    extra: {
+      code: 'fr',
+    },
+  },
+  {
+    name: 'Singapore',
+    value: 15088,
+  },
+  {
+    name: 'Austria',
+    value: 17371,
+  },
+  {
+    name: 'Palau',
+    value: 23431,
+  },
+  {
+    name: 'Viet Nam',
+    value: 19839,
+  },
+  {
+    name: 'Macedonia',
+    value: 56233,
+  },
+  {
+    name: 'Australia',
+    value: 42828,
+  },
+];
